d8a693004d4671789d0c3aa89335ea13a339b9d4
anandamayan <msanandamayan@gmail.com>
2021-12-21 22:04:02 +0530

    commit

---
+++ MyCode/src/busResv/Booking.java
@@ -0,0 +1,21 @@
+package busResv;
+
+import java.util.*;
+
+public class Booking {
+         String passengerName;
+         int busNo;
+         Date date;
+         
+         
+         Booking(){
+        	 Scanner scanner = new Scanner(System.in);
+        	 System.out.println("Enter name of Passenger: ");
+        	 passengerName = scanner.next();
+        	 System.out.println("Enter the Bus No: ");
+        	 busNo =  scanner.nextInt();
+        	 System.out.println("Enter date dd-mm-yyyy");
+        	 String dateInput = scanner.next();
+        	 date.
+         }
+}

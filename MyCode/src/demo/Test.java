245b9299fa9289e8b10e25a1ef74b1a7fbbef389
anandamayan <msanandamayan@gmail.com>
2021-12-21 22:24:54 +0530

    commit

---
+++ MyCode/src/demo/Test.java
@@ -0,0 +1,18 @@
+package demo;
+
+public class Test 
+{
+	Test()
+	{
+		System.out.println("Everforce");
+	}
+
+	public static void main(String[] args) {
+		Test t1 = new Test();
+		Test t2 = new Test();
+		Test t3 = new Test();
+		Test t4 = new Test();
+		
+	}
+
+}
